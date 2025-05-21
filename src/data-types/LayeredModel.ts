ca9978de0e3e251bd2620a04b71d4d2c4f1b1785
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2025-05-21 09:44:08 +0200

    feat: add model layer data structure

--- src/data-types/LayeredModel.ts
+++ src/data-types/LayeredModel.ts
@@ -1,14 +1,29 @@
 import {GORCNode} from "./GROCNodes";
 import {Question} from "./QuestionNodes";
 
+type ModelNode = Omit<GORCNode, "id" | "categories" | "subcategories" | "attributes" | "features"> & Child;
+
 export type LayeredModel = {
     nodes: {
-        [id: string]: Omit<GORCNode, "categories" | "subcategories" | "attributes" | "features"> & Child
+        [id: string]: ModelNode
     },
     questions: {
-        [id: string]: Question & Child
+        [id: string]: QuestionNode
     }
 }
+
+export type LayeredModelLayer = {
+    nodes: {
+        [id: string]: ModelNode | Nothing
+    },
+    questions: {
+        [id: string]: QuestionNode | Nothing
+    }
+}
+
+type QuestionNode = Omit<Question, "id"> & {type: "question"} & Child;
+
+type Nothing = {type: "nothing"}
 
 type Child = {
     pid: string | null;
